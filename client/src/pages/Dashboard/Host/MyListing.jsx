ad6bd928eaa46ba40351b77bbddc0e48342b1809
Azizul Rabby Chowdhury <anassust.scw@gmail.com>
2025-12-30 09:08:59 +0600

    delete implementation for room data row

--- client/src/pages/Dashboard/Host/MyListing.jsx
+++ client/src/pages/Dashboard/Host/MyListing.jsx
@@ -1,30 +1,52 @@
-import { useQuery } from '@tanstack/react-query'
-import { Helmet } from 'react-helmet-async'
-import LoadingSpinner from '../../../components/Shared/LoadingSpinner'
-import useAuth from '../../../hooks/useAuth'
-import useAxiosSecure from '../../../hooks/useAxiosSecure'
-import RoomDataRow from '../TableRows/RoomDataRow'
+import { useMutation, useQuery } from "@tanstack/react-query";
+import { Helmet } from "react-helmet-async";
+import LoadingSpinner from "../../../components/Shared/LoadingSpinner";
+import useAuth from "../../../hooks/useAuth";
+import useAxiosSecure from "../../../hooks/useAxiosSecure";
+import RoomDataRow from "../TableRows/RoomDataRow";
 
 const MyListings = () => {
+  const { user } = useAuth();
+  const axiosSecure = useAxiosSecure();
 
-   const { user } = useAuth()
-const axiosSecure = useAxiosSecure()
+  const {
+    data: rooms = [],
+    isLoading,
+    refetch,
+  } = useQuery({
+    queryKey: ["my-listings", user?.email],
+    enabled: !!user?.email,
+    queryFn: async () => {
+      const { data } = await axiosSecure.get("/my-listings", {
+        params: { email: user.email },
+      });
 
-const { data: rooms = [], isLoading, refetch } = useQuery({
-  queryKey: ['my-listings', user?.email],
-  enabled: !!user?.email,
-  queryFn: async () => {
-    const { data } = await axiosSecure.get('/my-listings', {
-      params: { email: user.email },
-    })
+      return data;
+    },
+  });
 
-    return data
+  //delete
+  const {mutateAsync} = useMutation({
+    mutationFn: async (roomId) => {
+      const response = await axiosSecure.delete(`/rooms/${roomId}`);
+      return response.data;
+    },
+    onSuccess: () => {
+      refetch();
+    },
+  });
 
-  },
-  
-})
 
-if (isLoading) return <LoadingSpinner />
+  // Handle Delete Button
+  const handleDelete = (roomId) => {
+    try {
+      mutateAsync(roomId);
+    } catch (error) {
+      console.error("Error deleting room:", error);
+    }
+  }
+
+  if (isLoading) return <LoadingSpinner />;
 
   return (
     <>
@@ -32,74 +54,69 @@ if (isLoading) return <LoadingSpinner />
         <title>My Listings</title>
       </Helmet>
 
-      <div className='container mx-auto px-4 sm:px-8'>
-        <div className='py-8'>
-          <div className='-mx-4 sm:-mx-8 px-4 sm:px-8 py-4 overflow-x-auto'>
-            <div className='inline-block min-w-full shadow rounded-lg overflow-hidden'>
-              <table className='min-w-full leading-normal'>
+      <div className="container mx-auto px-4 sm:px-8">
+        <div className="py-8">
+          <div className="-mx-4 sm:-mx-8 px-4 sm:px-8 py-4 overflow-x-auto">
+            <div className="inline-block min-w-full shadow rounded-lg overflow-hidden">
+              <table className="min-w-full leading-normal">
                 <thead>
                   <tr>
                     <th
-                      scope='col'
-                      className='px-5 py-3 bg-white  border-b border-gray-200 text-gray-800  text-left text-sm uppercase font-normal'
+                      scope="col"
+                      className="px-5 py-3 bg-white  border-b border-gray-200 text-gray-800  text-left text-sm uppercase font-normal"
                     >
                       Title
                     </th>
                     <th
-                      scope='col'
-                      className='px-5 py-3 bg-white  border-b border-gray-200 text-gray-800  text-left text-sm uppercase font-normal'
+                      scope="col"
+                      className="px-5 py-3 bg-white  border-b border-gray-200 text-gray-800  text-left text-sm uppercase font-normal"
                     >
                       Location
                     </th>
                     <th
-                      scope='col'
-                      className='px-5 py-3 bg-white  border-b border-gray-200 text-gray-800  text-left text-sm uppercase font-normal'
+                      scope="col"
+                      className="px-5 py-3 bg-white  border-b border-gray-200 text-gray-800  text-left text-sm uppercase font-normal"
                     >
                       Price
                     </th>
                     <th
-                      scope='col'
-                      className='px-5 py-3 bg-white  border-b border-gray-200 text-gray-800  text-left text-sm uppercase font-normal'
+                      scope="col"
+                      className="px-5 py-3 bg-white  border-b border-gray-200 text-gray-800  text-left text-sm uppercase font-normal"
                     >
                       From
                     </th>
                     <th
-                      scope='col'
-                      className='px-5 py-3 bg-white  border-b border-gray-200 text-gray-800  text-left text-sm uppercase font-normal'
+                      scope="col"
+                      className="px-5 py-3 bg-white  border-b border-gray-200 text-gray-800  text-left text-sm uppercase font-normal"
                     >
                       To
                     </th>
                     <th
-                      scope='col'
-                      className='px-5 py-3 bg-white  border-b border-gray-200 text-gray-800  text-left text-sm uppercase font-normal'
+                      scope="col"
+                      className="px-5 py-3 bg-white  border-b border-gray-200 text-gray-800  text-left text-sm uppercase font-normal"
                     >
                       Delete
                     </th>
                     <th
-                      scope='col'
-                      className='px-5 py-3 bg-white  border-b border-gray-200 text-gray-800  text-left text-sm uppercase font-normal'
+                      scope="col"
+                      className="px-5 py-3 bg-white  border-b border-gray-200 text-gray-800  text-left text-sm uppercase font-normal"
                     >
                       Update
                     </th>
                   </tr>
                 </thead>
                 <tbody>
-  {rooms.map((room) => (
-    <RoomDataRow
-      key={room._id}
-      room={room}
-      refetch={refetch}
-    />
-  ))}
-</tbody>
-
+                  {rooms.map((room) => (
+                    <RoomDataRow key={room._id} room={room} handleDelete={handleDelete} id={room?._id} />
+                  ))}
+                </tbody>
               </table>
             </div>
           </div>
         </div>
       </div>
     </>
-  )
-}
+  );
+};
 
-export default MyListings
+export default MyListings;
--- client/src/pages/Dashboard/TableRows/RoomDataRow.jsx
+++ client/src/pages/Dashboard/TableRows/RoomDataRow.jsx
@@ -1,6 +1,13 @@
 import { format } from 'date-fns'
+import DeleteModal from '../../../components/Modal/DeleteModal'
+import { useState } from 'react'
 
-const RoomDataRow = ({ room, refatch }) => {
+const RoomDataRow = ({ room, handleDelete, id }) => {
+  let [isOpen, setIsopen] = useState(false)
+  const closeModal = () => {
+    setIsopen(false)
+  }
+
   return (
     <tr>
       <td className='px-5 py-5 border-b border-gray-200 bg-white text-sm'>
@@ -10,7 +17,7 @@ const RoomDataRow = ({ room, refatch }) => {
               <img
                 alt='profile'
                 src={room?.image}
-                className='mx-auto object-cover rounded h-10 w-15 '
+                className='mx-auto object-cover rounded h-10 w-15'
               />
             </div>
           </div>
@@ -19,31 +26,43 @@ const RoomDataRow = ({ room, refatch }) => {
           </div>
         </div>
       </td>
+
       <td className='px-5 py-5 border-b border-gray-200 bg-white text-sm'>
         <p className='text-gray-900 whitespace-no-wrap'>{room?.location}</p>
       </td>
+
       <td className='px-5 py-5 border-b border-gray-200 bg-white text-sm'>
         <p className='text-gray-900 whitespace-no-wrap'>${room?.price}</p>
       </td>
+
       <td className='px-5 py-5 border-b border-gray-200 bg-white text-sm'>
         <p className='text-gray-900 whitespace-no-wrap'>
           {format(new Date(room?.from), 'P')}
         </p>
       </td>
+
       <td className='px-5 py-5 border-b border-gray-200 bg-white text-sm'>
         <p className='text-gray-900 whitespace-no-wrap'>
           {format(new Date(room?.to), 'P')}
         </p>
       </td>
+
+      {/* DELETE */}
       <td className='px-5 py-5 border-b border-gray-200 bg-white text-sm'>
-        <span className='relative cursor-pointer inline-block px-3 py-1 font-semibold text-green-900 leading-tight'>
+        <button
+        onClick={()=> setIsopen(true)}
+          className='relative cursor-pointer inline-block px-3 py-1 font-semibold text-green-900 leading-tight'
+        >
           <span
             aria-hidden='true'
             className='absolute inset-0 bg-red-200 opacity-50 rounded-full'
           ></span>
           <span className='relative'>Delete</span>
-        </span>
+          <DeleteModal closeModal={closeModal} isOpen={isOpen} handleDelete={handleDelete} id={id} />
+        </button>
       </td>
+
+      {/* UPDATE */}
       <td className='px-5 py-5 border-b border-gray-200 bg-white text-sm'>
         <span className='relative cursor-pointer inline-block px-3 py-1 font-semibold text-green-900 leading-tight'>
           <span
